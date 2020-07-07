21befaa51eab180fec638b2853581f7019048950
PundlaVnSwetha <puswetha@in.ibm.com>
2020-07-07 19:21:38 +0530

    Commit

--- AccountLoginService/src/main/resources/data.sql
+++ AccountLoginService/src/main/resources/data.sql
@@ -3,5 +3,4 @@ CREATE TABLE user ( id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20) NOT 
 INSERT INTO user (username, password) VALUES
 ('userone', '$2a$04$OrCDnLlCQLHWnmpFw8cvIOOxH3nY6vtVnS7PCqBC.FB.nBbZUDv1q'),
 ('usertwo', '$2a$04$PKJXGhfMZxP7Isfd8AsCg.sbFJQdtB3aX29Zp/GkRKRV18AsQR1gS'),
-('userthree', '$2a$04$RVN1tBvgwdREP3TYlPyr4uE6gaH6AMZPa031xYJtX.oBf6f9NGISW'),
-('userfour', '$2a$04$Fk6c3NbGX9VV2GwOBRrave23LHYrvXpWuRXIcvilwhVD5u.BkoE3K');
+('userthree', '$2a$04$RVN1tBvgwdREP3TYlPyr4uE6gaH6AMZPa031xYJtX.oBf6f9NGISW');
